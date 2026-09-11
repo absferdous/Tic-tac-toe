610745947f48266ae13f8aced014ce8b733cd991
absferdous <aferdous053@gmail.com>
2026-09-12 02:35:49 +0600

    added game moves

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const GameBoard = {
         console.log(this.count);
         const row = document.createElement("div");
         row.classList.add("row");
-        row.textContent = this.count;
+        // row.textContent = this.count;
         this.eventFunc(row);
+        this.setboxId(row);
+        console.log("id", row.id);
         col.append(row);
       }
       this.container.append(col);
@@ -21,15 +23,21 @@ const GameBoard = {
 
     console.log("gameboard created");
   },
-  eventFunc: function addEvent(element) {
+  eventFunc(element) {
     element.addEventListener("click", () => {
-      if (this.player1) {
-        element.textContent = "X";
-        this.player1 = false;
-      } else {
-        element.textContent = "O";
-        this.player1 = true;
+      if (!element.textContent) {
+        if (this.player1) {
+          element.textContent = "X";
+          this.player1 = false;
+          this.player1Move.push(element.id);
+        } else {
+          element.textContent = "O";
+          this.player1 = true;
+          this.player2Move.push(element.id);
+        }
+        console.log({ player1: this.player1Move, player2: this.player2Move });
       }
+
       //   this.player1 = false;
       //   this.player2 = true;
       //   alert(element.textContent);
@@ -37,6 +45,14 @@ const GameBoard = {
   },
   player1: true,
   player2: false,
+  setboxId(element) {
+    element.id = this.count;
+    return element.id;
+  },
+  player1Move: [],
+  player2Move: [],
 };
+
+const gameLogic = {};
 
 GameBoard.gameBoard();
